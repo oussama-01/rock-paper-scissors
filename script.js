dd4fbb406bc662cf3be9026afffaafa0b51ba1d4
oussama <120120579+oussama-01@users.noreply.github.com>
2023-02-12 14:39:24 +0100

    Create a function that returns one of 3 strings

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 
 
+/*A function that returns either: rock, paper or scissors*/
+
+function getComputerChoice() {
+
+    let randomNumber = Math.floor(Math.random() * 10);
+
+    if (randomNumber >= 0 && randomNumber < 4) {
+        return 'rock';
+
+    } else if (randomNumber >= 4 && randomNumber < 7) {
+        return 'paper';
+
+    } else {
+        return 'scissors';
+    }
+
+}
+
+
+
+console.log(getComputerChoice());
+
+
 
 
 
