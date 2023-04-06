ae468c86716c950d0704e3679e7ffd5ebb502f22
oussama <120120579+oussama-01@users.noreply.github.com>
2023-04-06 01:48:45 +0000

    Add a simple UI and the DOM logic

--- script.js
+++ script.js
@@ -20,9 +20,6 @@ function getComputerChoice() {
 
 /* A function that plays a single round of the game*/
         
-let playerSelection;
-let computerSelection = getComputerChoice();
-
 
     let loss;
     let win;
@@ -32,10 +29,10 @@ function playRound(playerSelection, computerSelection) {
 
     loss = `You lose! ${computerSelection} beats ${playerSelection}  `;
     win = `You win!  ${playerSelection} beats ${computerSelection}  `;
-    
+
     
         if (playerSelection === 'paper' && computerSelection === 'paper') {
-            return 'Even';
+            return 'Tie';
         } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
             return loss;
         } else if (playerSelection === 'paper' && computerSelection === 'rock') {
@@ -44,7 +41,7 @@ function playRound(playerSelection, computerSelection) {
     
     
         if (playerSelection === 'scissors' && computerSelection === 'scissors') {
-            return 'Even';
+            return 'Tie';
         } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
             return loss;
         } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
@@ -53,7 +50,7 @@ function playRound(playerSelection, computerSelection) {
     
     
         if (playerSelection === 'rock' && computerSelection === 'rock') {
-            return 'Even ';
+            return 'Tie';
     
         } else if (playerSelection === 'rock' && computerSelection === 'paper') {
             return loss;
@@ -64,25 +61,117 @@ function playRound(playerSelection, computerSelection) {
     }
 
 
-/* A functon that plays 5 rounds  */
+// below begin the using of the DOM for the UI
 
-// removed for the adding of a UI 
+let playerSelection = '';
+
+let computerSelection  ='';
+
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+
+const result =document.getElementById('result');
+
+let pScore = document.querySelector('#pScore');
+let cScore = document.querySelector('#cScore');
+
+const winner = document.querySelector('#winner');
+
+let playerScore= 0;
+let computerScore =0;
+
+pScore.textContent = `Your Score is: ${playerScore}`;
+cScore.textContent = `Computer score is: ${computerScore}`;
+
+rock.addEventListener('click', function() {
+
+    computerSelection = getComputerChoice();
+    playerSelection ='rock';
+    result.textContent =playRound(playerSelection, computerSelection);
+
+    showScore();
+    showWinner();
+
+});
+
+paper.addEventListener('click', function() {
+    computerSelection = getComputerChoice();
+    playerSelection = 'paper';
+    result.textContent =playRound(playerSelection, computerSelection);
+
+    showScore();
+    showWinner();
 
 
+});
+
+scissors.addEventListener('click', function() {
+    computerSelection =getComputerChoice();
+    playerSelection = 'scissors';
+    result.textContent = playRound(playerSelection, computerSelection);
+
+
+showScore();
+showWinner();
+});
+
+
+function showScore() {
+
+    /* incrementing scores */
+    if (playRound(playerSelection, computerSelection) === win) {
+        playerScore++;
+
+    } else if (playRound(playerSelection, computerSelection) === loss) {
+        computerScore++;
+    }
+    /* display the scores */
+
+    pScore.textContent = `Your Score is: ${playerScore}`;
+    cScore.textContent = `Computer score is: ${computerScore}`;
+
+}
+
+function showWinner() {
+
+    winner.textContent = '';
+    /* end the game when someone reaches 5 */
+
+    if (playerScore === 5) {
+
+        playerScore = 0;
+        computerScore = 0;
+        pScore.textContent = `Your Score is: ${playerScore}`;
+        cScore.textContent = `Computer score is: ${computerScore}`;
+
+        //console.log("you are the winner");
+        winner.innerHTML = " You are the winner";
+
+
+    } else if (computerScore === 5) {
+        playerScore = 0;
+        computerScore = 0;
+        pScore.textContent = `Your Score is: ${playerScore}`;
+        cScore.textContent = `Computer score is: ${computerScore}`;
+
+        //console.log("the computer is the winner");
+        winner.innerHTML = " The computer is the winner";
+
+    }
+
+
+}
+
+
+
+
+
+
+/* ******END******** */
 
 
 
 /* 
-
-
-
-
-
-
-
-
-
-
-
 
 */
